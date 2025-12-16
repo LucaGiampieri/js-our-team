611fee916373c920b05df8dd5d1bd11f0400f36a
Luca Giampieri <luca.giampieri95@gmail.com>
2025-12-16 17:55:37 +0100

    gestione del click e traformazione del compilatore dei membri in una funzione

--- JS/script.js
+++ JS/script.js
@@ -40,6 +40,9 @@ const teamMembers = [
 //dichiaro la variabile in cui andrò ad inserire le card
 const teamCards = document.getElementById('cardContainer');
 
+//invoco la funzione di compilazione membri
+compilerMembers(teamMembers,teamCards);
+
 //dichiaro le variabili di input del form
 const nameMember = document.getElementById('name');
 const roleMember = document.getElementById('role');
@@ -47,13 +50,34 @@ const emailMember = document.getElementById('email');
 const imageMember = document.getElementById('image');
 const form = document.getElementById('member-form');
 
+//gestione click form
+form.addEventListener("submit", function (event) {
+  //preveniamo comportamento di base form
+  event.preventDefault();
+  //recupero il valore dai campi
+  const name = nameMember.value;
+  const role = roleMember.value;
+  const email = emailMember.value;
+  const img = imageMember.value;
+  //creao un nuovo oggetto in cui inseire questi valori
+  const newMember = { name, role, email, img };
+  //insieriamo il nuovo membro nell'array dei membri originale
+  teamMembers.push(newMember);
+  //resetto tutti i campi del form
+  event.target.reset();
+  //invoco la funzione e ristampo tutti i membri più la nostra aggiunta
+  compilerMembers(teamMembers,teamCards);
+});
+
+//////FUNZIONI//////
+
+function compilerMembers (myInput,myOuput){
 //andiamo a crare una varibile cards dove andranno messe poi le singole card
 let cards = '';
-
 //vado a creare un ciclo che mi scorre l'array e che poi mi andrà a dare un output
-for (let i = 0; i < teamMembers.length; i++){
+for (let i = 0; i < myInput.length; i++) {
   //creo una variabile in cui salvare i teamMebers iesimi
-  const members = teamMembers[i];
+  const members = myInput[i];
   //faccio si che a ogni ciclo nella varibile cards venga aggiunta una card in scritta in HTML
   //vado ad inserire immagini, nomi, ruoli ed email
   cards += `<div class="card">
@@ -68,7 +92,5 @@ for (let i = 0; i < teamMembers.length; i++){
             </div>`
 }
 //vado a stamapre a schermo le cards
- teamCards.innerHTML = cards;
-
-
-
+teamCards.innerHTML = cards;
+}
